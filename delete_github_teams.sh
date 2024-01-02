bc2a9aa2eac17aaa1f0b50c2807b7f673615328e
Krishnadhas N K <108367225+githubofkrishnadhas@users.noreply.github.com>
2024-01-02 19:00:52 +0530

    Update delete_github_teams.sh

--- delete_github_teams.sh
+++ delete_github_teams.sh
@@ -8,7 +8,7 @@ list_all_team_details=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/$ORGANIZATION/teams
+  https://api.github.com/orgs/$ORGANIZATION/teams \
   -d "{\"per_page\":\"100\"}")
 
 
